fd32bb9c5b1c367b63fc7cbbf38b9b221d3206ba
Minjamin Ahmmed Sheefat <84741577+minjamin-ahmmed@users.noreply.github.com>
2025-07-10 17:41:39 +0600

    added vercel json file

--- server.js
+++ server.js
@@ -6,7 +6,12 @@ const port = process.env.PORT || 5000;
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "https://zyloo-client.vercel.app/"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zf7pxbf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
