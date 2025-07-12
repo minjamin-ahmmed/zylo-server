2860766e19c179054ed1559f0036719e3515fc82
Minjamin Ahmmed Sheefat <84741577+minjamin-ahmmed@users.noreply.github.com>
2025-07-12 13:06:10 +0600

    added right origin

--- api/index.js
+++ api/index.js
@@ -7,7 +7,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://zyloo-client.vercel.app"],
+    origin: ["http://localhost:5173", "https://zyloo-client.vercel.app/api"],
     credentials: true,
   })
 );
